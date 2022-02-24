131426824afe3061e5a3723611c60a9d17c668cc
Polina <polinka256@gmail.com>
2022-02-24 22:33:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let heap = [];
+  let collection = new Map(bracketsConfig);
+
+  str.split('').forEach((item, i) => {
+    if(item == collection.get(heap[heap.length - 1])) {
+      heap.pop();
+    } else {
+      heap.push(item);
+    }
+  })
+
+  return heap.length !== 0 ? false : true;
 }
